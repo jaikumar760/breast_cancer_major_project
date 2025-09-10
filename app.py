77ec091499abd46e7c36ca84029a141a71e5d44a
Jai kumar <jk4729954@gmail.com>
2025-09-10 14:32:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -281,5 +281,6 @@ def symptom_checker():
 with app.app_context():
     db.create_all()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
